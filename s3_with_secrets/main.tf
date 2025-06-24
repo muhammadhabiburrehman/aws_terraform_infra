5686959300a1357fd126302c93f6f935218ef139
Muhammad Habib <habib80246@gmail.com>
2025-06-24 14:59:58 +0500

    Added basic S3 Terraform setup

---
+++ s3_with_secrets/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.s3_bucket
+
+  tags = {
+    Name        = "My Secret bucket"
+    Environment = "Dev"
+  }
+}
